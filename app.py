e164e8aee1d58aa697bd94f708d7d13f00d18bf9
beurrekk <suphannika.kk@gmail.com>
2025-01-05 20:03:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+st.set_page_config(layout="wide")
+
 # Load the dataset
 data_file = 'test_data.csv'
 df = pd.read_csv(data_file)
